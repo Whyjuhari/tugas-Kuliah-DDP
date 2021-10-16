0a792d52a661ac732400d813ebaa714993f7d499
Whyjuhari <91404024+Whyjuhari@users.noreply.github.com>
2021-10-16 03:03:23 -0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,24 @@
+# Program biodata
+# Meminta input dari user
+
+
+
+print ("====================================================")
+print ("**********Biodata Sederhana Dengan Python***********")
+print ("====================================================")
+nama_lenkap = str(input("Masukkan nama lenkap anda       : "))
+umur        = int(input("Masukkan umur anda saat ini     : "))
+tinggiB     = int(input("Masukkan tinggi badan anda      : "))
+beratB      = float(input("Masukkan berat badan anda       : "))
+ukuran_baju = str(input("Masukkan ukuran baju anda       : "))
+status      = str(input("Masukkan status pernikahan anda : "))
+
+print("\n")
+print("============Data Diri Saya=============")
+print("Nama lenkap saya   : ", nama_lenkap)
+print("Saat ini umur saya : ", umur, "Tahun")
+print("Tinggi badan saya  : ", tinggiB, "Cm")
+print("Berat badan saya   : ", beratB, "Kg")
+print("Ukuran baju saya   : ", ukuran_baju)
+print("Dan status  saya   : ", status, "Yahh  Kasiaannnn ^_^")
+print("\n")
